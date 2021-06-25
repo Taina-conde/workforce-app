1a0454b545adb7260de934d5b34693b00b8e0c80
Taina Conde <tainabatistaconde@gmail.com>
2021-06-25 17:14:59 -0400

    feat: EmployeeForm - add setLocale from yup to show custom message when validating the form

--- src/components/EmployeeForm/index.js
+++ src/components/EmployeeForm/index.js
@@ -1,55 +1,69 @@
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import { useFormik } from "formik";
+import { setLocale } from "yup";
+import * as yup from "yup";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
 
+setLocale({
+  mixed: {
+    default: "Inválido",
+  },
+  number: {
+    min: "Deve conter 11 números",
+  },
+});
 const validationSchema = yup.object({
-    nome: yup
-      .string('Nome')
-      .nome('Insira um nome válido'),
-    cpf: yup
-      .number('CPF')
-      .min(11, 'O CPF deve conter 11 números'),
-  });
-  
+  nome: yup.string(),
+  cpf: yup.number().positive().integer().min(),
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
 const EmployeeForm = () => {
-    const formik = useFormik({
-        initialValues: {
-          
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-         
-        },
-      });
-    
-      return (
-        <div>
-          <form onSubmit={formik.handleSubmit}>
-            <TextField
-              id="nome"
-              name="nome"
-              label="Nome"
-              value={formik.values.nome}
-              onChange={formik.handleChange}
-              error={formik.touched.nome && Boolean(formik.errors.nome)}
-              helperText={formik.touched.nome && formik.errors.nome}
-            />
-            <TextField
-              id="cpf"
-              name="cpf"
-              label="CPF"
-              type="cpf"
-              value={formik.values.cpf}
-              onChange={formik.handleChange}
-              error={formik.touched.cpf && Boolean(formik.errors.cpf)}
-              helperText={formik.touched.cpf && formik.errors.cpf}
-            />
-            <Button color="primary" variant="contained" type="submit">
-              Pesquisar
-            </Button>
-          </form>
-        </div>
-      );
-}
+  const classes = useStyles();
+  const formik = useFormik({
+    initialValues: {
+      nome: "",
+      cpf: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {},
+  });
+
+  return (
+    <div>
+      <form className = {classes.root} onSubmit={formik.handleSubmit} autoComplete = "off">
+        <TextField
+          id="nome"
+          name="nome"
+          label="Nome"
+          value={formik.values.nome}
+          onChange={formik.handleChange}
+          error={formik.touched.nome && Boolean(formik.errors.nome)}
+          helperText={formik.touched.nome && formik.errors.nome}
+        />
+        <TextField
+          id="cpf"
+          name="cpf"
+          label="CPF"
+          type="cpf"
+          value={formik.values.cpf}
+          onChange={formik.handleChange}
+          error={formik.touched.cpf && Boolean(formik.errors.cpf)}
+          helperText={formik.touched.cpf && formik.errors.cpf}
+        />
+        <Button color="primary" variant="contained" type="submit">
+          Pesquisar
+        </Button>
+      </form>
+    </div>
+  );
+};
 export default EmployeeForm;
