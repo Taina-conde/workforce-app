e6dcb405c4152b0a1b77c8eb6cb0fd7c789cfbb7
Taina Conde <tainabatistaconde@gmail.com>
2021-06-25 16:19:28 -0400

    feat: Set up formik with material ui implementation
    Add the nome and cpf in the validationSchema and add the Textfield
    components related to these inputs.

--- src/components/EmployeeForm/index.js
+++ src/components/EmployeeForm/index.js
@@ -1,4 +1,55 @@
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+const validationSchema = yup.object({
+    nome: yup
+      .string('Nome')
+      .nome('Insira um nome válido'),
+    cpf: yup
+      .number('CPF')
+      .min(11, 'O CPF deve conter 11 números'),
+  });
+  
 const EmployeeForm = () => {
-    return ()
+    const formik = useFormik({
+        initialValues: {
+          
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+         
+        },
+      });
+    
+      return (
+        <div>
+          <form onSubmit={formik.handleSubmit}>
+            <TextField
+              id="nome"
+              name="nome"
+              label="Nome"
+              value={formik.values.nome}
+              onChange={formik.handleChange}
+              error={formik.touched.nome && Boolean(formik.errors.nome)}
+              helperText={formik.touched.nome && formik.errors.nome}
+            />
+            <TextField
+              id="cpf"
+              name="cpf"
+              label="CPF"
+              type="cpf"
+              value={formik.values.cpf}
+              onChange={formik.handleChange}
+              error={formik.touched.cpf && Boolean(formik.errors.cpf)}
+              helperText={formik.touched.cpf && formik.errors.cpf}
+            />
+            <Button color="primary" variant="contained" type="submit">
+              Pesquisar
+            </Button>
+          </form>
+        </div>
+      );
 }
 export default EmployeeForm;
