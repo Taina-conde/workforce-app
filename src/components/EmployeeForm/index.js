86183e67326f9ba39ffc0b185e7367e2a8a93890
Taina Conde <tainabatistaconde@gmail.com>
2021-06-25 17:57:25 -0400

    feat: Add Select Uf form control with UFs as MenuItems

--- src/components/EmployeeForm/index.js
+++ src/components/EmployeeForm/index.js
@@ -41,6 +41,7 @@ const EmployeeForm = () => {
       nome: "",
       cpf: "",
       cargo: "",
+      uf : "",
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {},
@@ -76,24 +77,61 @@ const EmployeeForm = () => {
           <InputLabel id="cargo">Cargo</InputLabel>
           <Select
             labelId="cargo"
-            id = "cargo"
-            name = "cargo"
+            id="cargo"
+            name="cargo"
             value={formik.values.cargo}
             onChange={formik.handleChange}
           >
-            <MenuItem  value={"devJr"}>Dev Jr</MenuItem>
-            <MenuItem  value={"devPl"}>Dev Pl</MenuItem>
+            <MenuItem value={"devJr"}>Dev Jr</MenuItem>
+            <MenuItem value={"devPl"}>Dev Pl</MenuItem>
             <MenuItem value={"devSr"}>Dev Sr</MenuItem>
-            <MenuItem  value={"POJr"}>PO Jr</MenuItem>
-            <MenuItem  value={"POPl"}>PO Pl</MenuItem>
-            <MenuItem  value={"POSr"}>PO Sr</MenuItem>
-            <MenuItem  value={"ACJr"}>AC Jr</MenuItem>
-            <MenuItem  value={"ACPl"}>AC Pl</MenuItem>
-            <MenuItem  value={"ACSr"}>AC Sr</MenuItem>
-            <MenuItem   value={"analistaJr"}>Analista Jr</MenuItem>
-            <MenuItem   value={"analistaPl"}>Analista Pl</MenuItem>
-            <MenuItem  value={"analistaSr"}>Analista Sr</MenuItem>
-                
+            <MenuItem value={"POJr"}>PO Jr</MenuItem>
+            <MenuItem value={"POPl"}>PO Pl</MenuItem>
+            <MenuItem value={"POSr"}>PO Sr</MenuItem>
+            <MenuItem value={"ACJr"}>AC Jr</MenuItem>
+            <MenuItem value={"ACPl"}>AC Pl</MenuItem>
+            <MenuItem value={"ACSr"}>AC Sr</MenuItem>
+            <MenuItem value={"analistaJr"}>Analista Jr</MenuItem>
+            <MenuItem value={"analistaPl"}>Analista Pl</MenuItem>
+            <MenuItem value={"analistaSr"}>Analista Sr</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="uf">Unidade Federativa</InputLabel>
+          <Select
+            labelId="uf"
+            id="uf"
+            name="uf"
+            value={formik.values.uf}
+            onChange={formik.handleChange}
+          >
+            <MenuItem value={"AC"}>AC</MenuItem>
+            <MenuItem value={"AL"}>AL</MenuItem>
+            <MenuItem value={"AP"}>AP</MenuItem>
+            <MenuItem value={"AM"}>AM</MenuItem>
+            <MenuItem value={"BA"}>BA</MenuItem>
+            <MenuItem value={"CE"}>CE</MenuItem>
+            <MenuItem value={"DF"}>DF</MenuItem>
+            <MenuItem value={"ES"}>ES</MenuItem>
+            <MenuItem value={"GO"}>GO</MenuItem>
+            <MenuItem value={"MA"}>MA</MenuItem>
+            <MenuItem value={"MT"}>MT</MenuItem>
+            <MenuItem value={"MS"}>MS</MenuItem>
+            <MenuItem value={"MG"}>MG</MenuItem>
+            <MenuItem value={"PA"}>PA</MenuItem>
+            <MenuItem value={"PB"}>PB</MenuItem>
+            <MenuItem value={"PR"}>PR</MenuItem>
+            <MenuItem value={"PE"}>PE</MenuItem>
+            <MenuItem value={"PI"}>PI</MenuItem>
+            <MenuItem value={"RJ"}>RJ</MenuItem>
+            <MenuItem value={"RJ"}>RN</MenuItem>
+            <MenuItem value={"RJ"}>RS</MenuItem>
+            <MenuItem value={"RJ"}>RO</MenuItem>
+            <MenuItem value={"RJ"}>RR</MenuItem>
+            <MenuItem value={"RJ"}>SC</MenuItem>
+            <MenuItem value={"RJ"}>SP</MenuItem>
+            <MenuItem value={"RJ"}>SE</MenuItem>
+            <MenuItem value={"RJ"}>TO</MenuItem>
           </Select>
         </FormControl>
         <Button color="primary" variant="contained" type="submit">
