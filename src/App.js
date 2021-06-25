020ab9da14b5e0248bfbd413124848ad232e104d
Taina Conde <tainabatistaconde@gmail.com>
2021-06-25 16:21:18 -0400

    feat: Render EmployeeForm in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Navbar from './components/Navbar';
+import EmployeeForm from './components/EmployeeForm';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <EmployeeForm/>
+
      
     </div>
   );
