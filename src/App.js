a3faa9526f2642b2add7cea36c4ebaba10a60b83
Taina Conde <tainabatistaconde@gmail.com>
2021-06-25 21:50:42 -0400

    fix: ResultsTable - remove extra export default at the bottom of the file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar';
 import EmployeeForm from './components/EmployeeForm';
+import ResultsTable from './components/ResultsTable';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <EmployeeForm/>
+      <ResultsTable/>
 
      
     </div>
--- src/components/ResultsTable/index.js
+++ src/components/ResultsTable/index.js
@@ -37,5 +37,3 @@ export default function ResultsTable() {
     </TableContainer>
   );
 }
-
-export default ResultsTable;
