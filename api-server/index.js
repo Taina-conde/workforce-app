fa6eb8a946215984a6266812114e8b6ebbdc3551
Taina Conde <tainabatistaconde@gmail.com>
2021-06-28 15:03:31 -0400

    feat: Api-server - Add authorization header get

--- api-server/index.js
+++ api-server/index.js
@@ -1,14 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const employees = require('./employees');
+const express = require("express");
+const cors = require("cors");
+const employees = require("./employees");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(cors());
 
-app.get('/',(req, res) => {
-    res.send("Hello World")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use((req, res, next) => {
+  const key = req.get("Authorization");
+
+  if (key) {
+    req.key = key;
+    next();
+  } else {
+    res.status(403).send({
+      error:
+        "Please, provide an Authorization header to identify yourself (can be whatever you want)",
+    });
+  }
+});
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
