91279d4bbbc74d28cb4200da15cb220ef86c13a5
Taina Conde <tainabatistaconde@gmail.com>
2021-06-28 15:41:37 -0400

    feat: Api-server - require config in index and refactor the app.listen to use the config

--- api-server/index.js
+++ api-server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const employees = require("./employees");
+const config = require('./config')
 
 const app = express();
 
@@ -50,5 +51,4 @@ app.get('/employees/:cpf', (req, res) => {
     )
 })
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(config.port, () => console.log(`Listening on port ${confi.port}, Ctrl + C to stop`));
