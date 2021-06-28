27581748d1ede0bc822c7b2c70ef7a34c241b21f
Taina Conde <tainabatistaconde@gmail.com>
2021-06-28 11:00:56 -0400

    feat: Api-server - create getAll and getByCpf functions
    The getAll function returns all the employees data coming
    from data.js file. Use key as a token to allow other people
    use their own authorization when consuming this api.

--- api-server/employees.js
+++ api-server/employees.js
@@ -1,3 +1,22 @@
-import {dataDefault} from "./data";
+import { dataDefault } from "./data";
+const clone = require("clone");
 
+let dataBase = {};
 
+function getAll(key) {
+  let data = dataBase[key];
+  if (data == null) {
+    data = dataBase[key] = clone(dataDefault);
+  }
+  return data;
+}
+
+function getByCpf(key, cpf) {
+  return new Promise((res) => {
+    let employees = getAll(key);
+    let filteredEmployees = employees.filter(
+      (employee) => employee.cpf === cpf
+    );
+    res(filteredEmployees)
+  });
+}
