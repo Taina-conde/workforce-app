29896a13492010fc218a3200f73dd41d4f3ec291
Taina Conde <tainabatistaconde@gmail.com>
2021-06-28 23:49:27 -0400

    feat: ResultRow - add HighlightOffIcon
    In the last column of each table row, add an icon button
    to delete that employee.

--- client/src/components/ResultsTable/ResultRow.js
+++ client/src/components/ResultsTable/ResultRow.js
@@ -1,19 +1,27 @@
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-const ResultRow = (props) => {
-    const {employee} = props;
-    return <TableRow>
-        <TableCell component = "th" scope = "row">
-            {employee.nome}
-        </TableCell>
-        <TableCell align = "right">{employee.cpf}</TableCell>
-        <TableCell align = "right">{employee.ufNasc}</TableCell>
-        <TableCell align = "right">{employee.dataCad}</TableCell>
-        <TableCell align = "right">{employee.cargo}</TableCell>
-        <TableCell align = "right">{employee.salario}</TableCell>
-        <TableCell align = "right">{employee.status}</TableCell>
-        <TableCell align = "right">DELETE BTN</TableCell>
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import IconButton from "@material-ui/core/IconButton";
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
+const ResultRow = (props) => {
+  const { employee } = props;
+  return (
+    <TableRow>
+      <TableCell component="th" scope="row">
+        {employee.nome}
+      </TableCell>
+      <TableCell align="right">{employee.cpf}</TableCell>
+      <TableCell align="right">{employee.ufNasc}</TableCell>
+      <TableCell align="right">{employee.dataCad}</TableCell>
+      <TableCell align="right">{employee.cargo}</TableCell>
+      <TableCell align="right">{employee.salario}</TableCell>
+      <TableCell align="right">{employee.status}</TableCell>
+      <TableCell align="right">
+        <IconButton aria-label="delete" color = "secondary">
+            <HighlightOffIcon/>
+        </IconButton>
+      </TableCell>
     </TableRow>
-}
+  );
+};
 export default ResultRow;
