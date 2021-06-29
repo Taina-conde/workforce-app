85e543ae78e8a4e25505ad3ff38359873b1bc15c
Taina Conde <tainabatistaconde@gmail.com>
2021-06-29 19:19:16 -0400

    style: InitalLogo - remove icon and change style
    Set logoStart to gray and with Fillate font. LogoEnd set to primary
    light color with bigger font size.

--- client/src/components/InitalLogo/index.js
+++ client/src/components/InitalLogo/index.js
@@ -1,31 +1,38 @@
 import { makeStyles } from "@material-ui/core/styles";
-import SearchIcon from "@material-ui/icons/Search";
 import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     fontSize: "5em",
-    height: "70vh"
+    height: "70vh",
   },
-  logoStart : {
-      textTransform: "lowercase",
-
-
+  logoStart: {
+    textTransform: "lowercase",
+    color: theme.palette.text.secondary,
+    fontFamily:
+      "'Fillate', 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
   },
-  logoEnd :  {
-      textTransform: "capitalize",
-      fontFamily: "'Fillate', 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
-  }
+  logoEnd: {
+    textTransform: "capitalize",
+    color: theme.palette.primary.light,
+    fontSize: "1.5em",
+  },
 }));
 
 export default function InitalLogo() {
   const classes = useStyles();
   return (
-    <Grid container className={classes.root} justify = "center" alignItems = "center">
-      <Grid item className = {classes.logoStart}>My</Grid>
-      <Grid item className = {classes.logoEnd}>employees</Grid>
-      <Grid item className = {classes.logoIcon}>
-        <SearchIcon fontSize = "inherit"/>
+    <Grid
+      container
+      className={classes.root}
+      justify="center"
+      alignItems="center"
+    >
+      <Grid item className={classes.logoStart}>
+        My
+      </Grid>
+      <Grid item className={classes.logoEnd}>
+        employees
       </Grid>
     </Grid>
   );
