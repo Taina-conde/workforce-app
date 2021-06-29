317adbe75ff45ac514cb572023fe572dcff0f955
Taina Conde <tainabatistaconde@gmail.com>
2021-06-29 18:59:14 -0400

    feat: InitalLogo - add Grid layout

--- client/src/components/InitalLogo/index.js
+++ client/src/components/InitalLogo/index.js
@@ -1,21 +1,27 @@
 import { makeStyles } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    textAlign: "center",
+    display: "flex",
+    fontSize: "5em",
+    height: "70vh"
   },
+  logoStart : {
+
+  }
 }));
 
 export default function InitalLogo() {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <span>My</span>
-      <span>Employees</span>
-      <span>
+    <Grid container className={classes.root} justify = "center" alignItems = "center">
+      <Grid item className = {classes.logoStart}>My</Grid>
+      <Grid item className = {classes.logoEnd}>employees</Grid>
+      <Grid item className = {classes.logoIcon}>
         <SearchIcon />
-      </span>
-    </div>
+      </Grid>
+    </Grid>
   );
 }
