f6da0cbec8f6aa75eb42cb73613fef89c2ed4512
Taina Conde <tainabatistaconde@gmail.com>
2021-06-29 19:44:29 -0400

    feat: Logo - Create new component in components shared folder
    Move the logic related to logo from InitalLogo to a new Logo
    component. This component is used in InitalLogo and Navbar.

--- client/src/components/InitalLogo/index.js
+++ client/src/components/InitalLogo/index.js
@@ -1,22 +1,10 @@
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Logo from "../shared/Logo";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    fontSize: "6em",
     height: "70vh",
-  },
-  logoStart: {
-    textTransform: "lowercase",
-    color: theme.palette.primary.light,
-    fontSize: "1.5em",
-    fontFamily:
-      "'Fillate', 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
-  },
-  logoEnd: {
-    textTransform: "capitalize",
-    color: theme.palette.text.secondary,
-    fontFamily : "monospace"
   },
 }));
 
@@ -29,11 +17,8 @@ export default function InitalLogo() {
       justify="center"
       alignItems="center"
     >
-      <Grid item className={classes.logoStart}>
-        Work
-      </Grid>
-      <Grid item className={classes.logoEnd}>
-        force
+      <Grid item>
+        <Logo />
       </Grid>
     </Grid>
   );
---
+++ client/src/components/shared/Logo/index.js
@@ -0,0 +1,38 @@
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    fontSize: "6em",
+  },
+  logoStart: {
+    textTransform: "lowercase",
+    color: theme.palette.primary.light,
+    fontSize: "1.5em",
+    fontFamily:
+      "'Fillate', 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
+  },
+  logoEnd: {
+    textTransform: "capitalize",
+    color: theme.palette.text.secondary,
+    fontFamily: "monospace",
+  },
+}));
+export default function Logo() {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      className={classes.root}
+      justify="center"
+      alignItems="center"
+    >
+      <Grid item className={classes.logoStart}>
+        Work
+      </Grid>
+      <Grid item className={classes.logoEnd}>
+        force
+      </Grid>
+    </Grid>
+  );
+}
