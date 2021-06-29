72924c9b239230d02a017a45657e3bd40022316c
Taina Conde <tainabatistaconde@gmail.com>
2021-06-29 18:40:50 -0400

    feat: InitalLogo - import SearchIcon

--- client/src/components/InitalLogo/index.js
+++ client/src/components/InitalLogo/index.js
@@ -1,14 +1,21 @@
 import { makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        textAlign: "center",
-    }
-  }));
+  root: {
+    textAlign: "center",
+  },
+}));
 
 export default function InitalLogo() {
-    const classes = useStyles();
-    return <div className = {classes.root}>
-
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <span>My</span>
+      <span>Employees</span>
+      <span>
+        <SearchIcon />
+      </span>
     </div>
+  );
 }
