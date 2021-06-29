a5af6779c02c1b556346f80f95b552a4c6faa9bc
Taina Conde <tainabatistaconde@gmail.com>
2021-06-28 23:54:43 -0400

    feat: ResultRow - clickDeleteHandler
    Create click handler to delete the correspondent employee.
    Call onDeleteEmployee method from the Context.

--- client/src/components/ResultsTable/ResultRow.js
+++ client/src/components/ResultsTable/ResultRow.js
@@ -2,9 +2,17 @@ import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import IconButton from "@material-ui/core/IconButton";
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import {useContext} from "react";
+import Context from "../../context";
 
 const ResultRow = (props) => {
   const { employee } = props;
+  const ctx = useContext(Context);
+
+  const clickDeleteHandler = () => {
+      ctx.onDeleteEmployee(employee.cpf);
+  }
+  
   return (
     <TableRow>
       <TableCell component="th" scope="row">
@@ -17,7 +25,7 @@ const ResultRow = (props) => {
       <TableCell align="right">{employee.salario}</TableCell>
       <TableCell align="right">{employee.status}</TableCell>
       <TableCell align="right">
-        <IconButton aria-label="delete" color = "secondary">
+        <IconButton aria-label="delete" color = "secondary" onClick = {clickDeleteHandler}>
             <HighlightOffIcon/>
         </IconButton>
       </TableCell>
