57413a6ecaf7333eac13d5c95c091668d46ad419
Taina Conde <tainabatistaconde@gmail.com>
2021-06-29 00:04:31 -0400

    feat: Api-server - add delete endpoint in employees/:cpf

--- api-server/index.js
+++ api-server/index.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const employees = require("./employees");
-const config = require('./config')
+const config = require("./config");
 
 const app = express();
 
@@ -16,68 +16,77 @@ app.get("/", (req, res) => {
 });
 
 app.use((req, res, next) => {
-    const token = req.get("Authorization");
-  
-    if (token) {
-      req.token = token;
-      next();
-    } else {
-      res.status(403).send({
-        error:
-          "Please, provide an Authorization header to identify yourself (can be whatever you want)",
+  const token = req.get("Authorization");
+
+  if (token) {
+    req.token = token;
+    next();
+  } else {
+    res.status(403).send({
+      error:
+        "Please, provide an Authorization header to identify yourself (can be whatever you want)",
+    });
+  }
+});
+
+app.get("/employees", (req, res) => {
+  employees.getAll(req.token).then(
+    (data) => res.send(data),
+    (error) => {
+      console.log(error);
+      res.status(500).send({
+        error: "There was an error.",
       });
     }
-  });
+  );
+});
+app.get("/employees/:criterioBusca/:query", (req, res) => {
+  console.log("req", req);
+  employees.getBy(req.token, req.params.criterioBusca, req.params.query).then(
+    (data) => res.send(data),
+    (error) => {
+      console.log(error);
+      res.status(500).send({
+        error: "There was an error.",
+      });
+    }
+  );
+});
+app.post("/employees", bodyParser.json(), (req, res) => {
+  employees.save(req.token, req.body).then(
+    (data) => res.send(data),
+    (error) => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error.",
+      });
+    }
+  );
+});
 
-app.get('/employees', (req, res) => {
-    employees.getAll(req.token)
-    .then(
-        (data) => res.send(data),
-        (error) => {
-            console.log(error);
-            res.status(500).send({
-                error: "There was an error."
-            })
-        }
-    )
-})
-app.get('/employees/:criterioBusca/:query', (req, res) => {
-    console.log('req', req)
-    employees.getBy(req.token, req.params.criterioBusca, req.params.query)
-    .then(
-        (data) => res.send(data),
-        (error) => {
-            console.log(error);
-            res.status(500).send({
-                error: 'There was an error.'
-            })
-        }
-    )
-})
-app.post('/employees', bodyParser.json(), (req, res) => {
-    employees.save(req.token, req.body)
-    .then(
-        (data) => res.send(data),
-        (error) => {
-            console.error(error)
-            res.status(500).send({
-               error: 'There was an error.'
-        })
-      }
-    )
-} )
+app.put("/employees/:cpf", bodyParser.json(), (req, res) => {
+  employees.save(req.token, req.params.cpf, req.body).then(
+    (data) => res.send(data),
+    (error) => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error.",
+      });
+    }
+  );
+});
+app.delete("/employees/:cpf", (req, res) => {
+  employees.disable(req.token, req.params.cpf).then(
+    (data) => res.send(data),
+    (error) => {
+      console.error(error);
+      res.status(500).send({
+        error: "There was an error.",
+      });
+    }
+  );
+});
 
-app.put('/employees/:cpf', bodyParser.json(), (req, res) => {
-    employees.save(req.token, req.params.cpf, req.body)
-    .then(
-        (data) => res.send(data),
-        (error) => {
-            console.error(error)
-            res.status(500).send({
-               error: 'There was an error.'
-        })
-      }
-    )
-})
-
-app.listen(config.port, () => console.log(`Listening on port ${config.port}, Ctrl + C to stop`));
+app.listen(config.port, () =>
+  console.log(`Listening on port ${config.port}, Ctrl + C to stop`)
+);
