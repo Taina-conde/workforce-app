8f70e9242b355deacd4506e0aef8643424243a0b
Taina Conde <tainabatistaconde@gmail.com>
2021-07-01 11:29:58 -0400

    test: App - add test to see if renders the logo when user has not started the search yet

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
 import App from './App';
 
-test('renders learn react link', () => {
+/*test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});*/
+test('renders the initial content logo when user has not started the search yet', () => {
+  const { getByText } = render(<App />);
+  //const logoText = screen.getByText('work');
+  expect(getByText(/work/i)).toBeInTheDocument();
 });
